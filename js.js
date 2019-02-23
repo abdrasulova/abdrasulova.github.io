7f9fdf3077a77031280a38a0416bf486bf8f2486
abdrasulova <galaxyaliyawka@gmail.com>
2019-02-23 18:31:34 +0600

    добавила JS

---
+++ js.js
@@ -0,0 +1,16 @@
+function fame() {
+	var a = prompt("Сколько будет 2 + 2?", '');
+	if (a == 4) {
+		alert('Правильно');
+	}else{
+		alert('Не Правильно');
+	}
+}
+function famous() {
+	 var b = prompt("Земля проская?", '');
+	 if (b == "нет") {
+	 	 alert('Правильно');
+	 }else{
+	 	alert('Не Правильно');
+	 }
+}
